28ae18bf0a95a2c8295008fb794f2efa3d479659
omidRR <omid.dastgahi1378@gmail.com>
2022-06-15 01:46:56 +0430

    Default Updated

---
+++ default.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="News_Magazine_Website._default" %>
+
+<!DOCTYPE html>
+<html>
+<head runat="server">
+    <title>در انتقال به صفحه اصلی</title>
+</head>
+<body>
+</body>
+</html>
--- default.aspx.cs
+++ default.aspx.cs
@@ -5,13 +5,14 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace News_Magazine_Website.Pages
+namespace News_Magazine_Website
 {
-    public partial class DefaultPage_Master : System.Web.UI.Page
+    public partial class _default : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            Response.Redirect("~/Pages/MainPage.Master.aspx");
+                
         }
     }
 }
