102931086e6612c13961622bcd6de7fe05bea595
omidRR <omid.dastgahi1378@gmail.com>
2022-06-15 02:21:55 +0430

    date

--- Master/mymaster.Master.cs
+++ Master/mymaster.Master.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace News_Magazine_Website
 {
@@ -12,7 +6,6 @@ namespace News_Magazine_Website
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
 
         }
     }
