29e23931dc269322729e0504cf9b774a316f8b07
omidRR <omid.dastgahi1378@gmail.com>
2022-06-16 03:57:21 +0430

    price update

---
+++ Pages/Price.aspx
@@ -0,0 +1,68 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master/mymaster.Master" AutoEventWireup="true" CodeBehind="Price.aspx.cs" Inherits="News_Magazine_Website.Pages.Price" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <table class="PriceTable">
+        <thead>
+        <tr>
+            <th>نماد</th>
+            <th>🇺🇸USD</th>
+            <th>🇪🇺EUR</th>
+            <th>🇦🇪AED</th>
+            <th>🇨🇳CNY</th>
+            <th>🇷🇺RUB</th>
+            <th>🇹🇷TRY</th>
+            <th>🇨🇦CAD</th>
+            <th>🇬🇧GBP</th>
+            <th>🇶🇦QAR</th>
+            <th>🇸🇦SAR</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>نرخ ارز</td>
+            <td><%: myDeserializedClass.data.prices.USD.current %></td>
+            <td><%: myDeserializedClass.data.prices.EUR.current %></td>
+            <td><%: myDeserializedClass.data.prices.AED.current %></td>
+            <td><%: myDeserializedClass.data.prices.CNY.current %></td>
+            <td><%: myDeserializedClass.data.prices.RUB.current %></td>
+            <td><%: myDeserializedClass.data.prices.TRY.current %></td>
+            <td><%: myDeserializedClass.data.prices.CAD.current %></td>
+            <td><%: myDeserializedClass.data.prices.GBP.current %></td>
+            <td><%: myDeserializedClass.data.prices.QAR.current %></td>
+            <td><%: myDeserializedClass.data.prices.SAR.current %></td>
+            
+        </tr>
+        <tr>
+            <td>تغییر قیمت</td>
+            <td><%: myDeserializedClass.data.prices.USD.changePrice %></td>
+            <td><%: myDeserializedClass.data.prices.EUR.changePrice %></td>
+            <td><%: myDeserializedClass.data.prices.AED.changePrice %></td>
+            <td><%: myDeserializedClass.data.prices.CNY.changePrice %></td>
+            <td><%: myDeserializedClass.data.prices.RUB.changePrice %></td>
+            <td><%: myDeserializedClass.data.prices.TRY.changePrice %></td>
+            <td><%: myDeserializedClass.data.prices.CAD.changePrice %></td>
+            <td><%: myDeserializedClass.data.prices.GBP.changePrice %></td>
+            <td><%: myDeserializedClass.data.prices.QAR.changePrice %></td>
+            <td><%: myDeserializedClass.data.prices.SAR.changePrice %></td>
+        </tr>
+        <tr>
+            <td>درصد تغییر</td>
+            <td><%: myDeserializedClass.data.prices.USD.changePercent %>%</td>
+            <td><%: myDeserializedClass.data.prices.EUR.changePercent %>%</td>
+            <td><%: myDeserializedClass.data.prices.AED.changePercent %>%</td>
+            <td><%: myDeserializedClass.data.prices.CNY.changePercent %>%</td>
+            <td><%: myDeserializedClass.data.prices.RUB.changePercent %>%</td>
+            <td><%: myDeserializedClass.data.prices.TRY.changePercent %>%</td>
+            <td><%: myDeserializedClass.data.prices.CAD.changePercent %>%</td>
+            <td><%: myDeserializedClass.data.prices.GBP.changePercent %>%</td>
+            <td><%: myDeserializedClass.data.prices.QAR.changePercent %>%</td>
+            <td><%: myDeserializedClass.data.prices.SAR.changePercent %>%</td>
+        </tr>
+        </tbody>
+    </table>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+</asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="ContentPlaceHolder3" runat="server">
+</asp:Content>
---
+++ Pages/Price.aspx.cs
@@ -0,0 +1,380 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace News_Magazine_Website.Pages
+{
+    public partial class Price : System.Web.UI.Page
+    {
+        public prices.Root myDeserializedClass;
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {  RestClient client23 = new RestClient();
+            client23.UserAgent =
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36";
+            RestRequest request = new RestRequest(Method.GET);
+            request.Timeout = 10000;
+            client23 = new RestClient("http://shamimsoft.ir/mahdi/arz/arz.txt");
+            IRestResponse response = client23.Execute(request);
+            response.Content=response.Content.Replace("+", "🔺").Replace("-", "🔻");
+            myDeserializedClass = JsonConvert.DeserializeObject<prices.Root>(response.Content);
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                Response.Write("خطا در دریافت نرخ");
+                return;
+            }
+            //Response.Write(myDeserializedClass.data.prices.USD.current);
+
+
+        }
+    }
+
+    public class prices
+    {
+        // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+        public class AED
+        {
+            public string min { get; set; }
+            public string max { get; set; }
+            public string current { get; set; }
+            public string changePercent { get; set; }
+            public string changePrice { get; set; }
+        }
+
+        public class AFN
+        {
+            public string min { get; set; }
+            public string max { get; set; }
+            public string current { get; set; }
+            public string changePercent { get; set; }
+            public string changePrice { get; set; }
+        }
+
+        public class AMD
+        {
+            public string min { get; set; }
+            public string max { get; set; }
+            public string current { get; set; }
+            public string changePercent { get; set; }
+            public string changePrice { get; set; }
+        }
+
+        public class AUD
+        {
+            public string min { get; set; }
+            public string max { get; set; }
+            public string current { get; set; }
+            public string changePercent { get; set; }
+            public string changePrice { get; set; }
+        }
+
+        public class AZN
+        {
+            public string min { get; set; }
+            public string max { get; set; }
+            public string current { get; set; }
+            public string changePercent { get; set; }
+            public string changePrice { get; set; }
+        }
+
+        public class BHD
+        {
+            public string min { get; set; }
+            public string max { get; set; }
+            public string current { get; set; }
+            public string changePercent { get; set; }
+            public string changePrice { get; set; }
+        }
+
+        public class CAD
+        {
+            public string min { get; set; }
+            public string max { get; set; }
+            public string current { get; set; }
+            public string changePercent { get; set; }
+            public string changePrice { get; set; }
+        }
+
+        public class CHF
+        {
+            public string min { get; set; }
+            public string max { get; set; }
+            public string current { get; set; }
+            public string changePercent { get; set; }
+            public string changePrice { get; set; }
+        }
+
+        public class CNY
+        {
+            public string min { get; set; }
+            public string max { get; set; }
+            public string current { get; set; }
+            public string changePercent { get; set; }
+            public string changePrice { get; set; }
+        }
+
+        public class Data
+        {
+            public string message { get; set; }
+            public int status { get; set; }
+            public Prices prices { get; set; }
+        }
+
+        public class DKK
+        {
+            public string min { get; set; }
+            public string max { get; set; }
+            public string current { get; set; }
+            public string changePercent { get; set; }
+            public string changePrice { get; set; }
+        }
+
+        public class EUR
+        {
+            public string min { get; set; }
+            public string max { get; set; }
+            public string current { get; set; }
+            public string changePercent { get; set; }
+            public string changePrice { get; set; }
+        }
+
+        public class GBP
+        {
+            public string min { get; set; }
+            public string max { get; set; }
+            public string current { get; set; }
+            public string changePercent { get; set; }
+            public string changePrice { get; set; }
+        }
+
+        public class GEL
+        {
+            public string min { get; set; }
+            public string max { get; set; }
+            public string current { get; set; }
+            public string changePercent { get; set; }
+            public string changePrice { get; set; }
+        }
+
+        public class HKD
+        {
+            public string min { get; set; }
+            public string max { get; set; }
+            public string current { get; set; }
+            public string changePercent { get; set; }
+            public string changePrice { get; set; }
+        }
+
+        public class INR
+        {
+            public string min { get; set; }
+            public string max { get; set; }
+            public string current { get; set; }
+            public string changePercent { get; set; }
+            public string changePrice { get; set; }
+        }
+
+        public class IQD
+        {
+            public string min { get; set; }
+            public string max { get; set; }
+            public string current { get; set; }
+            public string changePercent { get; set; }
+            public string changePrice { get; set; }
+        }
+
+        public class JPY
+        {
+            public string min { get; set; }
+            public string max { get; set; }
+            public string current { get; set; }
+            public string changePercent { get; set; }
+            public string changePrice { get; set; }
+        }
+
+        public class KWD
+        {
+            public string min { get; set; }
+            public string max { get; set; }
+            public string current { get; set; }
+            public string changePercent { get; set; }
+            public string changePrice { get; set; }
+        }
+
+        public class MYR
+        {
+            public string min { get; set; }
+            public string max { get; set; }
+            public string current { get; set; }
+            public string changePercent { get; set; }
+            public string changePrice { get; set; }
+        }
+
+        public class NOK
+        {
+            public string min { get; set; }
+            public string max { get; set; }
+            public string current { get; set; }
+            public string changePercent { get; set; }
+            public string changePrice { get; set; }
+        }
+
+        public class NZD
+        {
+            public string min { get; set; }
+            public string max { get; set; }
+            public string current { get; set; }
+            public string changePercent { get; set; }
+            public string changePrice { get; set; }
+        }
+
+        public class OMR
+        {
+            public string min { get; set; }
+            public string max { get; set; }
+            public string current { get; set; }
+            public string changePercent { get; set; }
+            public string changePrice { get; set; }
+        }
+
+        public class PKR
+        {
+            public string min { get; set; }
+            public string max { get; set; }
+            public string current { get; set; }
+            public string changePercent { get; set; }
+            public string changePrice { get; set; }
+        }
+
+        public class Prices
+        {
+            public USD USD { get; set; }
+            public EUR EUR { get; set; }
+            public GBP GBP { get; set; }
+            public AED AED { get; set; }
+            public TRY TRY { get; set; }
+            public CNY CNY { get; set; }
+            public JPY JPY { get; set; }
+            public CAD CAD { get; set; }
+            public AUD AUD { get; set; }
+            public NZD NZD { get; set; }
+            public CHF CHF { get; set; }
+            public AFN AFN { get; set; }
+            public SEK SEK { get; set; }
+            public DKK DKK { get; set; }
+            public NOK NOK { get; set; }
+            public KWD KWD { get; set; }
+            public SAR SAR { get; set; }
+            public QAR QAR { get; set; }
+            public OMR OMR { get; set; }
+            public IQD IQD { get; set; }
+            public BHD BHD { get; set; }
+            public SYP SYP { get; set; }
+            public INR INR { get; set; }
+            public PKR PKR { get; set; }
+            public SGD SGD { get; set; }
+            public HKD HKD { get; set; }
+            public MYR MYR { get; set; }
+            public THB THB { get; set; }
+            public RUB RUB { get; set; }
+            public AZN AZN { get; set; }
+            public AMD AMD { get; set; }
+            public GEL GEL { get; set; }
+        }
+
+        public class QAR
+        {
+            public string min { get; set; }
+            public string max { get; set; }
+            public string current { get; set; }
+            public string changePercent { get; set; }
+            public string changePrice { get; set; }
+        }
+
+        public class Root
+        {
+            public Data data { get; set; }
+        }
+
+        public class RUB
+        {
+            public string min { get; set; }
+            public string max { get; set; }
+            public string current { get; set; }
+            public string changePercent { get; set; }
+            public string changePrice { get; set; }
+        }
+
+        public class SAR
+        {
+            public string min { get; set; }
+            public string max { get; set; }
+            public string current { get; set; }
+            public string changePercent { get; set; }
+            public string changePrice { get; set; }
+        }
+
+        public class SEK
+        {
+            public string min { get; set; }
+            public string max { get; set; }
+            public string current { get; set; }
+            public string changePercent { get; set; }
+            public string changePrice { get; set; }
+        }
+
+        public class SGD
+        {
+            public string min { get; set; }
+            public string max { get; set; }
+            public string current { get; set; }
+            public string changePercent { get; set; }
+            public string changePrice { get; set; }
+        }
+
+        public class SYP
+        {
+            public string min { get; set; }
+            public string max { get; set; }
+            public string current { get; set; }
+            public string changePercent { get; set; }
+            public string changePrice { get; set; }
+        }
+
+        public class THB
+        {
+            public string min { get; set; }
+            public string max { get; set; }
+            public string current { get; set; }
+            public string changePercent { get; set; }
+            public string changePrice { get; set; }
+        }
+
+        public class TRY
+        {
+            public string min { get; set; }
+            public string max { get; set; }
+            public string current { get; set; }
+            public string changePercent { get; set; }
+            public string changePrice { get; set; }
+        }
+
+        public class USD
+        {
+            public string min { get; set; }
+            public string max { get; set; }
+            public string current { get; set; }
+            public string changePercent { get; set; }
+            public string changePrice { get; set; }
+        }
+
+
+    }
+}
